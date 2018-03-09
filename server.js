7a2832335c65aa3a0e8422f210502db3b76c6d77
woojunchoi <tim.woojune1@gmail.com>
2018-03-09 15:07:36 -0800

    folderchange

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const expressGraphQL = require('express-graphql')
+
 const app = express()
+
+app.use('/graphql', expressGraphQL({
+    schema:schema,
+    graphiql:true
+}))
 
 app.listen(4000, () => {
     console.log('listening to 4000')
