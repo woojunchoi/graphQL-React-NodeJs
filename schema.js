564ad065e76b4db143b2ce9e681728ef43c0217f
woojunchoi <tim.woojune1@gmail.com>
2018-03-10 02:56:54 -0800

    created hard coded json data, and querying data through graphql

--- schema.js
+++ schema.js
@@ -7,6 +7,49 @@ const {
     GraphQLNonNull
 } = require('graphql')
 
-module.exports = new GraphQLSchem({
+//hardcoded data
 
+const customer = [
+    {id:'1', name:'John Doe', email:'johndoe@gmail.com', age:35},
+    {id:'2', name:'Steve Smith', email:'stevesmith@gmail.com', age:25},
+    {id:'3', name:'Sarah Williams', email:'sarah@gmail.com', age:32}
+]
+
+const CustomerType = new GraphQLObjectType({
+    name:'customer',
+    fields: () => ({
+        id:{type:GraphQLString},
+        name:{type:GraphQLString},
+        email:{type:GraphQLString},
+        age:{type:GraphQLInt}
+    })
+})
+//Root Query
+const RootQuery = new GraphQLObjectType({
+    name:'rootQueryType',
+    fields: {
+        customer:{
+        type:CustomerType,
+        args:{
+            id:{type:GraphQLString}
+        },
+        resolve(parentValue,args) {
+            for(let i=0; i<customer.length; i++) {
+                if(customer[i].id === args.id) {
+                    return customer[i]
+                }
+            }
+            
+        }
+    },
+        customers: {
+            type: new GraphQLList(CustomerType),
+            resolve(parentValue, args) {
+            return customer
+        }
+    }
+}
+})
+module.exports = new GraphQLSchema({
+    query:RootQuery
 })
