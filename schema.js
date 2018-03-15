e4d4551dc954dd05e2a334604989ef8ac7d5fa4b
woojunchoi <tim.woojune1@gmail.com>
2018-03-15 00:04:57 -0700

    practice for complex resolves

--- schema.js
+++ schema.js
@@ -11,49 +11,59 @@ const {
     GraphQLNonNull
 } = require('graphql')
 
-//hardcoded data
-
-
-const CustomerType = new GraphQLObjectType({
-    name:'customer',
-    fields: () => ({
-        id:{type:GraphQLInt},
-        username:{type:GraphQLString},
-        email:{type:GraphQLString},
+const UserType = new GraphQLObjectType({
+    name:'User',
+    field: () => ({
+        id:{type:GraphQLString},
+        firstname:{type:GraphQLString},
         age:{type:GraphQLInt}
     })
 })
-//Root Query
-const RootQuery = new GraphQLObjectType({
-    name:'rootQueryType',
-    fields: {
-        customer:{
-        type:CustomerType,
-        args:{
-            id:{type:GraphQLInt}
-        },
-        resolve(parentValue,args) {
-           const query = `SELECT * FROM "user" WHERE id = ${args.id}`
-           return db.conn.one(query)
-           .then((data) => {
-               return data
-           })
-           .catch((error) => {
-               return error;
-           })
-        }
-    },
-        customers: {
-            type: new GraphQLList(CustomerType),
-            resolve(parentValue, args) {
-            const query = `SELECT * FROM "user"`
-            return db.conn.many(query)
-            .then(data => data)
-            .catch(err => err)
-        }
-    }
-}
-})
-module.exports = new GraphQLSchema({
-    query:RootQuery
-})
+
+
+
+
+
+// const CustomerType = new GraphQLObjectType({
+//     name:'customer',
+//     fields: () => ({
+//         id:{type:GraphQLInt},
+//         username:{type:GraphQLString},
+//         email:{type:GraphQLString},
+//         age:{type:GraphQLInt}
+//     })
+// })
+// //Root Query
+// const RootQuery = new GraphQLObjectType({
+//     name:'rootQueryType',
+//     fields: {
+//         customer:{
+//         type:CustomerType,
+//         args:{
+//             id:{type:GraphQLInt}
+//         },
+//         resolve(parentValue,args) {
+//            const query = `SELECT * FROM "user" WHERE id = ${args.id}`
+//            return db.conn.one(query)
+//            .then((data) => {
+//                return data
+//            })
+//            .catch((error) => {
+//                return error;
+//            })
+//         }
+//     },
+//         customers: {
+//             type: new GraphQLList(CustomerType),
+//             resolve(parentValue, args) {
+//             const query = `SELECT * FROM "user"`
+//             return db.conn.many(query)
+//             .then(data => data)
+//             .catch(err => err)
+//         }
+//     }
+// }
+// })
+// module.exports = new GraphQLSchema({
+//     query:RootQuery
+// })
